9c581cd6bb20c720a4f122a6c723a4428fb6c81f
ChristinaGaitan <christina.gaitan@gmail.com>
2021-02-19 09:33:42 -0600

    Create views and urls for Post CRUD and drafts

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.AboutView.as_view(), name='about')
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('/about', views.AboutView.as_view(), name='about'),
+    path('/post/<slug:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('/post/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('/post/<slug:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('/post/<slug:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('/drafts/', views.DraftListView.as_view(), name='post_draft_list'),
 ]
