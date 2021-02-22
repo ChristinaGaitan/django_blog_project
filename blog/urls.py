34315d014adc65a37cfac4c442a57f737650b4eb
ChristinaGaitan <christina.gaitan@gmail.com>
2021-02-22 09:18:29 -0600

    Create view and url for remove comment

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('/post/<slug:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
     path('/drafts/', views.DraftListView.as_view(), name='post_draft_list'),
     path('/post/<slug:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
-    path('/post/<slug:pk>/comment_approve/', views.comment_approve, name='comment_approve'),
+    path('/post/<slug:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('/post/<slug:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
